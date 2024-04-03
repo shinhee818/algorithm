399be6588b222c844498bbe839b795bc3b2dcc4b
shinhee818 <xbox234@naver.com>
2024-04-03 22:37:11 +0900

    [bakjoon]-8958

---
+++ baekjoon/baekjoon1000.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        Long collect = Long.valueOf(Arrays.stream(scanner.nextLine().split(" "))
+        .map(Integer::parseInt)
+                        .reduce(0, Integer::sum));
+
+        System.out.println(collect);
+    }
+}
